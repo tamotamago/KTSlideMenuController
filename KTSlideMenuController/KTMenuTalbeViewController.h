1a8a4d96c581ea2d6722827ca7777abb8099ea4e
tamotamago <denden_k_t_810@yahoo.co.jp>
2012-07-25 12:59:55 +0900

    implement slide button

---
+++ KTSlideMenuController/KTMenuTalbeViewController.h
@@ -0,0 +1,13 @@
+//
+//  KTMenuTalbeViewController.h
+//  KTSlideMenuController
+//
+//  Created by Tamura Koya on 12/07/24.
+//  Copyright (c) 2012年 Koya Tamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KTMenuTalbeViewController : UITableViewController
+
+@end
--- KTSlideMenuController/KTViewController.m
+++ KTSlideMenuController/KTViewController.m
@@ -7,17 +7,37 @@
 //
 
 #import "KTViewController.h"
+#import "KTMenuTalbeViewController.h"
 
 @interface KTViewController ()
+
+@property (nonatomic, assign) BOOL isOpen;
+
+@property (nonatomic, retain) IBOutlet UIView *dummyView;
 
 @end
 
 @implementation KTViewController
 
+@synthesize isOpen =_isOpen;
+
+@synthesize dummyView = _dummyView;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    KTMenuTalbeViewController *menuTableViewController = 
+    [self.storyboard instantiateViewControllerWithIdentifier:@"KTMenuTalbeViewController"];
+    [menuTableViewController.view setFrame:_dummyView.frame];
+    
+    [self addChildViewController:menuTableViewController];
+    [menuTableViewController didMoveToParentViewController:self];
+    [self.view addSubview:menuTableViewController.view];
+    
+    UIViewController *viewController1 = [self.storyboard instantiateViewControllerWithIdentifier:@"ViewController1"];
+    [self setFirstViewController:viewController1];
 }
 
 - (void)viewDidUnload
@@ -29,6 +49,44 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+#pragma mark - setFirstViewController
+-(void)setFirstViewController:(UIViewController *)viewController
+{
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+    UIBarButtonItem *leftBarButtonItem = [[UIBarButtonItem alloc] 
+                                          initWithTitle:@"menu" 
+                                          style:UIBarButtonItemStylePlain 
+                                          target:self 
+                                          action:@selector(slide:)];
+    [viewController.navigationItem setLeftBarButtonItem:leftBarButtonItem];
+    //[navigationController.navigationItem setLeftBarButtonItem:leftBarButtonItem];
+    [self addChildViewController:navigationController];
+    [navigationController didMoveToParentViewController:self];
+    [self.view addSubview:navigationController.view];
+}
+
+-(void)removeFirstViewController
+{
+
+}
+
+#pragma mark - slide
+-(void)slide:(id)sender
+{
+    _isOpen = !_isOpen;
+    
+    UINavigationController *firstNavigationController = [self.childViewControllers objectAtIndex:1];
+    UIView *firstView = firstNavigationController.view;
+    [UIView animateWithDuration:0.3 animations:^{
+        CGFloat originX = _isOpen ? 200 : 0;
+        CGRect frame = firstView.frame;
+        frame.origin.x = originX;
+        firstView.frame = frame;         
+    } completion:^(BOOL finished){
+        
+    }];
 }
 
 @end
